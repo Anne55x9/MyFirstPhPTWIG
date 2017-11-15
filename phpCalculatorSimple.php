1f018ebc969a2eb82de694f47a1081e963922d69
Anne Sofie Molsted Wanscher <anne55x9@edu.easj.dk>
2017-11-15 11:39:49 +0100

    Initial commit

---
+++ phpCalculatorSimple.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ASW
+ * Date: 01/11/2017
+ * Time: 13:52
+ */
+
+$a = $_REQUEST["a"];
+$b = $_REQUEST["b"];
+$operation = $_REQUEST["operation"];
+switch ($operation){
+    case "+":
+        $result = $a + $b;
+        break;
+    case "-":
+        $result = $a - $b;
+        break;
+    case "*":
+        $result = $a * $b;
+        break;
+    case "/":
+        $result = $a / $b;
+        break;
+    default:
+        $result = "Illigal operation:" + $operation;
+}
+$longResult = $a . $operation . $b . "=" . $result;
+
+
+require_once 'vendor/autoload.php';
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Calculator result</title>
+</head>
+<body>
+<?= $longResult ?>
+</body>
+</html>
